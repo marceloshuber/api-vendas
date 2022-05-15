2f49f5eede278cf1e1b10968389101b7debb1ef6
Marcelo S. Huber <marceloshuber@gmail.com>
2022-05-15 18:54:41 -0300

    Forgot Password Route

--- src/modules/users/routes/password.routes.ts
+++ src/modules/users/routes/password.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 import { celebrate, Joi, Segments } from 'celebrate';
 import ForgotPasswordController from '../controllers/ForgotpasswordController';
+import { messages } from 'joi-translation-pt-br';
 
 const passwordRouter = Router();
 const forgotPasswordController = new ForgotPasswordController();
+
+console.log('Router password aecssada');
 
 passwordRouter.post(
     '/forgot',
